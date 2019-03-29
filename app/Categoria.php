82690e9fe497a465270a6653080547663e8cb2a0
Duke <juan.carlos@cceo.com.mx>
2019-03-28 21:41:52 -0600

    Agregue el modelo articulo y agregue funciones a el modelo articulo y categoria

--- app/Categoria.php
+++ app/Categoria.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    protected $table = 'categorias';
-    protected $primaryKey ='id';
+   // protected $table = 'categorias';
+    //protected $primaryKey ='id';
     protected $fillable=['nombre','descripcion','condicion'];
+    public function articulos(){
+        return $this->hasMany('App\Articulo');
+    }
 }
