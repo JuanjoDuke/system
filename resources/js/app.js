4129cf4d682c5c1c1917e4b6fbe9341ebd686190
Duke <juan.carlos@cceo.com.mx>
2019-03-29 18:54:50 -0600

    Agregue el componente Cliente con cambios en el codigo de categorias para adaptarlo a clintes, agregue el componente a app js y lo asigne en content blade

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,7 +22,7 @@ window.Vue = require('vue');
 
 Vue.component('categoria-component', require('./components/Categoria.vue').default);
 Vue.component('articulo-component', require('./components/Articulo.vue').default);
-
+Vue.component('cliente-component', require('./components/Cliente.vue').default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
