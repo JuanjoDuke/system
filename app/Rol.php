e967998bff3c02780e05b8edf1aa57d6b5f95e29
Duke <juan.carlos@cceo.com.mx>
2019-03-30 13:12:11 -0600

    guardamos cambios anteriores faltantes

--- app/Rol.php
+++ app/Rol.php
@@ -9,6 +9,7 @@ class Rol extends Model
     protected $table = 'roles';
     protected $fillable = ['nombre','descripcion','condicion'];
     public $timestamps = false;
+    
     public function users()
     {
         return $this->hasMany('App\User');
