65038ec11708f155e18f2c0d91d9ac55f79a6911
Duke <juan.carlos@cceo.com.mx>
2019-04-01 15:52:12 -0600

    Agregamos el objeto v select, el cual instalamos primero, creamos su metodo en el controlador proveedores y lo implementamos en ingresos vue

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::group(['middleware'=>['auth']],function(){
         Route::get('/proveedor', 'ProveedorController@index');
         Route::post('/proveedor/registrar', 'ProveedorController@store');
         Route::put('/proveedor/actualizar', 'ProveedorController@update');
+        Route::get('/proveedor/selectProveedor', 'ProveedorController@selectProveedor');
 
         Route::get('/ingreso', 'IngresoController@index');
         Route :: post('/ingreso/registrar','IngresoController@store');
@@ -70,6 +71,7 @@ Route::group(['middleware'=>['auth']],function(){
         Route::get('/proveedor', 'ProveedorController@index');
         Route::post('/proveedor/registrar', 'ProveedorController@store');
         Route::put('/proveedor/actualizar', 'ProveedorController@update');
+        Route::get('/proveedor/selectProveedor', 'ProveedorController@selectProveedor');
 
         Route::get('/cliente', 'ClienteController@index');
         Route::post('/cliente/registrar', 'ClienteController@store');
