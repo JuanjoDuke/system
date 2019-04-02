5b36458809bb1a926756a8e9710022fd0e54564a
Duke <juan.carlos@cceo.com.mx>
2019-04-02 17:39:01 -0600

    Agregamos el componente Venta vue y modificamos la paginacion

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,6 +27,7 @@ Vue.component('proveedor-component', require('./components/Proveedor.vue').defau
 Vue.component('rol-component', require('./components/Rol.vue').default);
 Vue.component('user-component', require('./components/User.vue').default);
 Vue.component('ingreso-component', require('./components/Ingreso.vue').default);
+Vue.component('venta-component', require('./components/Venta.vue').default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
